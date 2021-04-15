04365afb2090a8ce4e314c32b1cf3a0009f91523
JD Pablo <jdpablo1002@gmail.com>
2021-04-15 19:01:29 -0400

    connected bot to discord server, added hi response

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const Discord = require('discord.js');
 require('dotenv').config();
+
+const client = new Discord.Client();
+client.on('ready', () => {
+    console.log('Bot is ready');
+});
+client.login(process.env.BOT_TOKEN)
+
+client.on('message', (msg) => {
+    if (msg.content === 'Hello ICDB!') msg.reply('Hi :)');
+});
