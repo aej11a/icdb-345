ff0768cd1be516db4450dc095ec4ab1abf57a96e
JD Pablo <jdpablo1002@gmail.com>
2021-04-15 19:56:18 -0400

    added steps to run in README.md, added time/date functionalities

--- app.js
+++ app.js
@@ -3,10 +3,32 @@ require('dotenv').config();
 
 const client = new Discord.Client();
 client.on('ready', () => {
-    console.log('Bot is ready');
+    console.log('The bot is connected to Yang\'s Gang!');
 });
 client.login(process.env.BOT_TOKEN)
 
 client.on('message', (msg) => {
     if (msg.content === 'Hello ICDB!') msg.reply('Hi :)');
+
+    if(msg.content == '!date') {
+        let date = new Date();
+        let content = 'Today\'s date is ' + date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear() + '.';
+        msg.channel.send(content)
+    }
+
+    if(msg.content == '!time') {
+        let date = new Date();
+        var pm = false;
+        var hours = date.getHours();
+        var amOrPm = 'am';
+        if(date.getHours() > 12) {
+            pm = true;
+            hours = date.getHours() - 12;
+        }
+        if(pm) {
+            amOrPm = 'pm';
+        }
+        let content = 'The time is ' + hours + ':' + date.getMinutes() + ':' + date.getSeconds() + ' ' + amOrPm + '.';
+        msg.channel.send(content)
+    }
 });
