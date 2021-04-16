af1b53ac319429d7071ab83b14cd49fa70b133f1
JD Pablo <jdpablo1002@gmail.com>
2021-04-15 20:18:47 -0400

    fixed js rules rule-breaking, fixed time formatting

--- app.js
+++ app.js
@@ -10,21 +10,29 @@ client.login(process.env.BOT_TOKEN)
 client.on('message', (msg) => {
     if (msg.content === 'Hello ICDB!') msg.reply('Hi :)');
 
-    if(msg.content == '!date') {
-        let date = new Date();
-        let content = 'Today\'s date is ' + date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear() + '.';
+    if(msg.content === '!date') {
+        const date = new Date();
+        const content = 'Today\'s date is ' + date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear() + '.';
         msg.channel.send(content)
     }
 
-    if(msg.content == '!time') {
-        let date = new Date();
-        var hours = date.getHours();
-        var amOrPm = 'am';
+    if(msg.content === '!time') {
+        const date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        let amOrPm = 'am';
         if(date.getHours() > 12) {
             hours = date.getHours() - 12;
-            amOrPm = 'pm'
+            amOrPm = 'pm';
         }
-        let content = 'The time is ' + hours + ':' + date.getMinutes() + ':' + date.getSeconds() + ' ' + amOrPm + '.';
+        if(date.getMinutes() < 10){
+            minutes = '0' + date.getMinutes();
+        }
+        if(date.getSeconds() < 10){
+            seconds = '0' + date.getSeconds();
+        }
+        const content = 'The time is ' + hours + ':' + minutes + ':' + seconds + ' ' + amOrPm + '.';
         msg.channel.send(content)
     }
 });
