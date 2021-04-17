929ae495ab583d073b46ed12adb29e8431bbaa0d
Andrew Jones <aej11@hotmail.com>
2021-04-16 21:04:24 -0400

    Added eslint and prettier tooling

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-const Discord = require('discord.js');
-require('dotenv').config();
+const Discord = require('discord.js')
+require('dotenv').config()
+
+const tripme = 'test'
+
+console.log('test')
